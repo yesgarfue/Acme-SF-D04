610cfc683f1ae417fbc88eab9c49f0a0e01d7674
lidia <lidjimsor@alum.us.es>
2024-02-26 11:18:46 +0100

    Creación de carpetas en entities

--- src/main/java/acme/entities/audits/AuditRecords.java
+++ src/main/java/acme/entities/audits/AuditRecords.java
@@ -1,5 +1,5 @@
 
-package acme.entities;
+package acme.entities.audits;
 
 import java.time.Duration;
 
--- src/main/java/acme/entities/banner/Banner.java
+++ src/main/java/acme/entities/banner/Banner.java
@@ -1,5 +1,5 @@
 
-package acme.entities;
+package acme.entities.banner;
 
 import java.util.Date;
 
