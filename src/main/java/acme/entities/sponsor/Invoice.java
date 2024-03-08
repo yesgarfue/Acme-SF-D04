3e24b0283f569fb3b12809c7156ff09c07d7ef1a
Yesica Garate <yesgarfue@alum.us.es>
2024-03-08 06:35:23 +0100

    Task S04: Update Sponsor

--- src/main/java/acme/entities/sponsor/Invoice.java
+++ src/main/java/acme/entities/sponsor/Invoice.java
@@ -5,17 +5,20 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.PositiveOrZero;
 
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
+import acme.client.data.datatypes.Money;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -29,32 +32,42 @@ public class Invoice extends AbstractEntity {
 	private static final long	serialVersionUID	= 1L;
 
 	//------------------------------------------------------------------------
-
 	@NotBlank
+	@Pattern(regexp = "^IN-[0-9]{4}-[0-9]{4}$")
 	@Column(unique = true)
-	@Pattern(regexp = "IN-[0-9]{4}-[0-9]{4}")
 	private String				code;
 
+	@NotNull
 	@Past
 	@Temporal(TemporalType.TIMESTAMP)
-	@NotNull
-	private Date				recordTime;
+	private Date				registrationTime;
 
+	@NotNull
 	@Temporal(TemporalType.TIMESTAMP)
+	private Date				dueDate;
+
 	@NotNull
-	private Date				expirationDate;
+	@Valid
+	private Money				quantity;
 
-	@PositiveOrZero
-	private int					amount;
-
-	@PositiveOrZero
+	@Min(0)
 	private double				tax;
 
 	@URL
 	private String				link;
 
+	// Derived attributes -----------------------------------------------------
 
-	public double totalAmount() {
-		return this.amount + this.tax / 100 * this.amount;
+
+	private double totalAmount() {
+		return this.quantity.getAmount() + this.quantity.getAmount() * (this.tax / 100);
 	}
+
+	// Relationships ----------------------------------------------------------
+
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Sponsorship sponsorship;
 }
