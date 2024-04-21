022e266544dbdf612d28597682bb1d6fa57ebdb2
Yesica Garate <yesgarfue@alum.us.es>
2024-04-21 14:32:45 +0200

    D03-Task-S4: Update of the sponsor's UPDATE service to display

---
+++ src/main/java/acme/features/sponsor/sponsorship/SponsorSponsorshipUpdateService.java
@@ -0,0 +1,91 @@
+
+package acme.features.sponsor.sponsorship;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.data.models.Dataset;
+import acme.client.services.AbstractService;
+import acme.client.views.SelectChoices;
+import acme.entities.projects.Project;
+import acme.entities.sponsor.Sponsorship;
+import acme.enumerate.SponsorshipType;
+import acme.roles.Sponsor;
+
+@Service
+public class SponsorSponsorshipUpdateService extends AbstractService<Sponsor, Sponsorship> {
+
+	@Autowired
+	private SponsorSponsorshipRepository repository;
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Sponsorship object;
+		int id = super.getRequest().getData("id", int.class);
+
+		object = this.repository.findSponsorshipById(id);
+
+		super.getBuffer().addData(object);
+	}
+
+	@Override
+	public void bind(final Sponsorship object) {
+		assert object != null;
+
+		int projectId;
+		Project project;
+
+		projectId = super.getRequest().getData("project", int.class);
+		project = this.repository.findOneProjectById(projectId);
+
+		super.bind(object, "code", "moment", "startDate", "finishDate", "amount", "sponsorshipType", "email", "link");
+		object.setProject(project);
+	}
+
+	@Override
+	public void validate(final Sponsorship object) {
+		assert object != null;
+
+		//AGREGAR MÁS RESTRICCIONES
+		if (!super.getBuffer().getErrors().hasErrors("code")) {
+			Sponsorship exist;
+
+			exist = this.repository.findOneSponsorshipByCode(object.getCode());
+			super.state(exist == null, "code", "sponsor.sponsorship.form.error.duplicated");
+		}
+	}
+
+	@Override
+	public void perform(final Sponsorship object) {
+		assert object != null;
+		this.repository.save(object);
+	}
+
+	@Override
+	public void unbind(final Sponsorship object) {
+		assert object != null;
+
+		Collection<Project> projects;
+		SelectChoices choices, choicesEnum;
+		Dataset dataset;
+
+		projects = this.repository.findAllProjects();
+		choices = SelectChoices.from(projects, "code", object.getProject());
+		choicesEnum = SelectChoices.from(SponsorshipType.class, object.getSponsorshipType());
+
+		dataset = super.unbind(object, "code", "moment", "startDate", "finishDate", "amount", "email", "link");
+		dataset.put("project", choices.getSelected().getKey());
+		dataset.put("sponsorshipType", choicesEnum);
+		dataset.put("projects", choices);
+
+		super.getResponse().addData(dataset);
+	}
+}
