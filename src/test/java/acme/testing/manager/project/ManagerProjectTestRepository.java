d0fead54b80b2ec612d10f6ddbdbe726321bf952
ALVARO RUIZ <alvruigut@alum.us.es>
2024-05-06 14:34:31 +0200

    Testing student 1

---
+++ src/test/java/acme/testing/manager/project/ManagerProjectTestRepository.java
@@ -0,0 +1,8 @@
+
+package acme.testing.manager.project;
+
+import acme.client.repositories.AbstractRepository;
+
+public interface ManagerProjectTestRepository extends AbstractRepository {
+
+}
---
+++ src/test/java/acme/testing/manager/userStory/ManagerUserStoryPublishTest.java
@@ -0,0 +1,8 @@
+
+package acme.testing.manager.userStory;
+
+import acme.testing.TestHarness;
+
+public class ManagerUserStoryPublishTest extends TestHarness {
+
+}
