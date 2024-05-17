322c0cc64a64960c216029fd94668fda8f521130
Yesica Garate <yesgarfue@alum.us.es>
2024-05-17 19:40:00 +0200

    Update: Creation of controller and services for currency exchange

--- src/main/java/acme/features/authenticated/moneyExchange/AuthenticatedMoneyExchangeController.java
+++ src/main/java/acme/features/authenticated/moneyExchange/AuthenticatedMoneyExchangeController.java
@@ -1,6 +1,22 @@
+
 package acme.features.authenticated.moneyExchange;
 
+import javax.annotation.PostConstruct;
 
-public class AuthenticatedMoneyExchangeController {
+import org.springframework.beans.factory.annotation.Autowired;
 
+import acme.client.controllers.AbstractController;
+import acme.client.data.accounts.Authenticated;
+import acme.forms.MoneyExchange;
+
+public class AuthenticatedMoneyExchangeController extends AbstractController<Authenticated, MoneyExchange> {
+
+	@Autowired
+	private AuthenticatedMoneyExchangePerformService performService;
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addBasicCommand("perform", this.performService);
+	}
 }
--- src/main/java/acme/features/authenticated/moneyExchange/AuthenticatedMoneyExchangePerformService.java
+++ src/main/java/acme/features/authenticated/moneyExchange/AuthenticatedMoneyExchangePerformService.java
@@ -1,6 +1,13 @@
+
 package acme.features.authenticated.moneyExchange;
 
+import org.springframework.stereotype.Service;
 
-public class AuthenticatedMoneyExchangePerformService {
+import acme.client.data.accounts.Authenticated;
+import acme.client.services.AbstractService;
+import acme.forms.MoneyExchange;
+
+@Service
+public class AuthenticatedMoneyExchangePerformService extends AbstractService<Authenticated, MoneyExchange> {
 
 }
