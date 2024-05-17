d02a29beb5b373b9adff5669fffdead653ddcfe4
lidia <lidjimsor@alum.us.es>
2024-05-17 12:07:45 +0200

    Coverage

--- src/main/java/acme/features/developer/trainingModule/DeveloperTrainingModuleDeleteService.java
+++ src/main/java/acme/features/developer/trainingModule/DeveloperTrainingModuleDeleteService.java
@@ -6,14 +6,9 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import acme.client.data.models.Dataset;
-import acme.client.helpers.PrincipalHelper;
 import acme.client.services.AbstractService;
-import acme.client.views.SelectChoices;
-import acme.entities.projects.Project;
 import acme.entities.training.TrainingModule;
 import acme.entities.training.TrainingSession;
-import acme.enumerate.Difficulty;
 import acme.roles.Developer;
 
 @Service
@@ -73,28 +68,31 @@ public class DeveloperTrainingModuleDeleteService extends AbstractService<Develo
 		this.repository.delete(object);
 	}
 
-	@Override
-	public void unbind(final TrainingModule object) {
-		assert object != null;
-
-		Dataset dataset;
-		SelectChoices choices;
-		SelectChoices choices2;
-		final Collection<Project> projects = this.repository.findAllProject();
-
-		choices = SelectChoices.from(projects, "code", object.getProject());
-		choices2 = SelectChoices.from(Difficulty.class, object.getDifficultyLevel());
-		dataset = super.unbind(object, "code", "creationMoment", "details", "updatedMoment", "optionalLink", "totalTime", "draftMode");
-		dataset.put("difficultyLevels", choices2);
-		dataset.put("project", choices.getSelected().getKey());
-		dataset.put("projects", choices);
-		super.getResponse().addData(dataset);
-
-	}
-
-	@Override
-	public void onSuccess() {
-		if (super.getRequest().getMethod().equals("POST"))
-			PrincipalHelper.handleUpdate();
-	}
+	/*
+	 * @Override
+	 * public void unbind(final TrainingModule object) {
+	 * assert object != null;
+	 * 
+	 * Dataset dataset;
+	 * SelectChoices choices;
+	 * SelectChoices choices2;
+	 * final Collection<Project> projects = this.repository.findAllProject();
+	 * 
+	 * choices = SelectChoices.from(projects, "code", object.getProject());
+	 * choices2 = SelectChoices.from(Difficulty.class, object.getDifficultyLevel());
+	 * dataset = super.unbind(object, "code", "creationMoment", "details", "updatedMoment", "optionalLink", "totalTime", "draftMode");
+	 * dataset.put("difficultyLevels", choices2);
+	 * dataset.put("project", choices.getSelected().getKey());
+	 * dataset.put("projects", choices);
+	 * super.getResponse().addData(dataset);
+	 * 
+	 * }
+	 * 
+	 * 
+	 * @Override
+	 * public void onSuccess() {
+	 * if (super.getRequest().getMethod().equals("POST"))
+	 * PrincipalHelper.handleUpdate();
+	 * }
+	 */
 }
